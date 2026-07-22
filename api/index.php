e014b91860f0d0f97259a6e46e5077cd4d840a1e
forinjkt88 <forinjkt88@gmail.com>
2026-07-22 14:26:52 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,7 +1,6 @@
 <?php
-echo "Current dir: " . __DIR__ . "<br>";
-echo "Path file: " . realpath('data.json') . "<br>";
-$data = json_decode(file_get_contents('./data.json'), true);
+$data = json_decode(file_get_contents(__DIR__ . '/data.json'), true);
+$data = json_decode(file_get_contents('data.json'), true);
 $slug = $_GET['id'] ?? 'willy';
 
 if (!isset($data[$slug])) {
